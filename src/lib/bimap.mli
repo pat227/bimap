a4bf22db3af32c3ba7fad95f41ee912e0ca5a18e
paul <paul@guerriere>
2018-02-19 19:29:53 -0500

    Works. Syntax needed tweaking; could not see how to do this without refs.  Changes to be committed: 	modified:   src/lib/bimap.ml 	modified:   src/lib/bimap.mli

--- src/lib/bimap.mli
+++ src/lib/bimap.mli
@@ -1,4 +1,17 @@
 module Bimap : sig
- (* include (module type of Core.Map)
-  val inverse : unit -> t	    *)
-end 
+  class ['a, 'b] bimap_class :
+          ('a, 'b, 'c) Core.Map.t ->
+          ('b, 'a, 'd) Core.Map.t ->
+          object
+            val mutable forward_map : ('a, 'b, 'c) Core.Map.t ref
+            val mutable reverse_map : ('b, 'a, 'd) Core.Map.t ref
+            method add : 'a -> 'b -> unit
+            method add_inverse : key:'a -> data:'b -> unit
+            method find : key:'a -> 'b Core_kernel__.Import.option
+            method find_exn : key:'a -> 'b
+            method find_exn_inverse : key:'b -> 'a
+            method find_inverse : key:'b -> 'a Core_kernel__.Import.option
+            method is_empty : Core_kernel__.Import.bool
+            method length : Core_kernel__.Import.int
+          end
+end
