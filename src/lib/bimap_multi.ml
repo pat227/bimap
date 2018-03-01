fd97fd82b1db047d4e3eb2cac8b48296d25bdfab
paul <paul@guerriere>
2018-03-01 18:51:27 -0500

    Progress on multi version of bimap. Compiles so far; needs a basic test.  Changes to be committed: 	modified:   src/lib/bimap_multi.ml 	modified:   src/lib/bimap_multi.mli

--- src/lib/bimap_multi.ml
+++ src/lib/bimap_multi.ml
@@ -1,32 +1,35 @@
 (*do this twice; once with objects/classes and once with modules
   --m1 and m2 are empty maps that need to be provided by client code-- *)
 module Bimap_multi = struct
-  class ['a,'b] bimap_class m1 m2 = object
+  class ['a,'b] bimap_class m1 m2 = object(self)
     val mutable forward_map = ref m1
     val mutable reverse_map = ref m2 
     method add_multi ~(key:'a) ~(data:'b) =
       let () = forward_map := (Core.Map.add_multi !forward_map ~key ~data) in
       reverse_map := Core.Map.add !reverse_map ~key:data ~data:key
-(*    method add_inverse ~key ~data =
-      let () = reverse_map := (Core.Map.add !reverse_map ~key ~data) in
-      forward_map := Core.Map.add !forward_map ~key:data ~data:key
+    method add_multi_inverse ~(key:'b) ~(data:'a) =
+      let () = forward_map := (Core.Map.add_multi !forward_map ~key:data ~data:key) in
+      reverse_map := Core.Map.add !reverse_map ~key ~data
+    method private add_inverse_values k v =
+      let rec add_values k v =
+	match k with
+	| h :: t ->
+	   let () = reverse_map := (Core.Map.add !reverse_map ~key:h ~data:v) in
+	   add_values t v
+	| [] -> () in
+      add_values k v 		  
     method change ~key ~f =
-      let old_value = Core.Map.find_exn !forward_map key in 
+      let old_value_list = Core.Map.find_exn !forward_map key in 
       let () = forward_map := (Core.Map.change !forward_map key ~f) in
-      let new_value = Core.Map.find_exn !forward_map key in 
-      let () = reverse_map := (Core.Map.add !reverse_map ~key:new_value ~data:key) in
-      reverse_map := (Core.Map.remove !reverse_map old_value)
+      let new_values = Core.Map.find_exn !forward_map key in
+      let () = self#add_inverse_values new_values key in 
+      self#remove_inverse_keys old_value_list		  
     method change_inverse ~key ~f =
       let old_key = Core.Map.find_exn !reverse_map key in 
       let () = reverse_map := (Core.Map.change !reverse_map key ~f) in
       let new_value = Core.Map.find_exn !reverse_map key in 
-      let () = forward_map := (Core.Map.add !forward_map ~key:new_value ~data:key) in
+      let () = forward_map := (Core.Map.add_multi !forward_map ~key:new_value ~data:key) in
       forward_map := (Core.Map.remove !forward_map old_key)
-    (*Cannot do these right now--type 'c the compraator is unbound
-    method comparator () =
-      Core.Map.comparator !forward_map
-    method comparator_inverse () =
-      Core.Map.comparator !reverse_map*)
     method data =
       Core.Map.data !forward_map
     method data_inverse =
@@ -62,6 +65,35 @@ module Bimap_multi = struct
       Core.Map.keys !reverse_map
     method length =
       Core.Map.length !forward_map
+    method mem key =
+      Core.Map.mem !forward_map key
+    method mem_inverse key =
+      Core.Map.mem !reverse_map key
+    method remove ~key =
+      let reverse_keys = Core.Map.find_exn !forward_map key in 
+      let () = forward_map := (Core.Map.remove !forward_map key) in
+      self#remove_inverse_keys reverse_keys
+    method private remove_inverse_keys klist = 
+      let rec remove_keys k =
+	match k with
+	| h :: t ->
+	   let () = reverse_map := (Core.Map.remove !reverse_map h) in
+	   remove_keys t
+	| [] -> () in
+      remove_keys klist
+    method remove_inverse ~key =
+      let fwd_key = Core.Map.find_exn !reverse_map key in 
+      let () = reverse_map := (Core.Map.remove !reverse_map key) in
+      forward_map := (Core.Map.remove !forward_map fwd_key)
+
+(*
+
+    (*Cannot do these right now--type 'c the compraator is unbound
+    method comparator () =
+      Core.Map.comparator !forward_map
+    method comparator_inverse () =
+      Core.Map.comparator !reverse_map*)
+
     method map ~f =
       let () = forward_map := (Core.Map.map !forward_map ~f) in
       let () = reverse_map := Core.Map.empty ~comparator:(Core.Map.comparator !reverse_map) in
@@ -90,18 +122,6 @@ module Bimap_multi = struct
 	!reverse_map
 	~f:(fun k -> forward_map :=
 		       Core.Map.add !forward_map ~key:(Core.Map.find_exn !reverse_map k) ~data:k)
-    method mem key =
-      Core.Map.mem !forward_map key
-    method mem_inverse key =
-      Core.Map.mem !reverse_map key
-    method remove ~key =
-      let reverse_key = Core.Map.find_exn !forward_map key in 
-      let () = forward_map := (Core.Map.remove !forward_map key) in
-      reverse_map := (Core.Map.remove !reverse_map reverse_key)
-    method remove_inverse ~key =
-      let fwd_key = Core.Map.find_exn !reverse_map key in 
-      let () = reverse_map := (Core.Map.remove !reverse_map key) in
-      forward_map := (Core.Map.remove !forward_map fwd_key)
     method update key ~f =
       let oldvalue = Core.Map.find_exn !forward_map key in
       let () = forward_map := (Core.Map.update !forward_map key ~f) in
--- src/lib/bimap_multi.mli
+++ src/lib/bimap_multi.mli
@@ -1,11 +1,44 @@
 module Bimap_multi : sig
   class ['a, 'b] bimap_class :
-	  ('a, 'b Core_kernel__.Import.list, 'c) Core.Map.t ->
+	  ('a, 'b list, 'c) Core.Map.t ->
 	  ('b, 'a, 'd) Core.Map.t ->
 	  object
             val mutable forward_map :
-			  ('a, 'b Core_kernel__.Import.list, 'c) Core.Map.t ref
+			  ('a, 'b list, 'c) Core.Map.t ref
             val mutable reverse_map : ('b, 'a, 'd) Core.Map.t ref
             method add_multi : key:'a -> data:'b -> unit
+            method add_multi_inverse : key:'b -> data:'a -> unit
+	    method change : key:'a -> f:('b list option ->
+					 'b list option) -> unit
+	    method change_inverse : key:'b -> f:('a option ->
+						 'a option) -> unit
+            method data : 'b list list
+            method data_inverse : 'a list
+            method empty : unit -> unit
+	    method find :
+		     key:'a -> 'b list option
+            method find_exn : key:'a -> 'b list
+            method find_exn_inverse : key:'b -> 'a
+            method find_inverse : key:'b -> 'a option
+	    method is_empty : bool
+            method iter :
+		     f:('b list -> unit) -> unit
+            method iter_inverse :
+		     f:('a -> unit) -> unit
+            method iter_keys :
+		     f:('a -> unit) -> unit
+            method iter_keys_inverse :
+		     f:('b -> unit) -> unit
+            method iteri :
+		     f:(key:'a -> data:'b list -> unit) -> unit
+            method iteri_inverse :
+		     f:(key:'b -> data:'a -> unit) -> unit
+	    method keys : 'a list
+            method keys_inverse : 'b list
+            method length : int
+	    method mem : 'a -> bool
+            method mem_inverse : 'b -> bool
+	    method remove : key:'a -> unit
+            method remove_inverse : key:'b -> unit
 	  end
 end
