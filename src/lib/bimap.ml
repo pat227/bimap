5955949c8147e3ed3477aea56a370d7702b506b4
paul <paul@guerriere>
2018-03-02 22:54:17 -0500

    Throwing in more functions...  Changes to be committed: 	modified:   src/lib/bimap.ml 	modified:   src/lib/bimap.mli

--- src/lib/bimap.ml
+++ src/lib/bimap.ml
@@ -39,6 +39,12 @@ module Bimap = struct
 		 forward_map :=
 		   Core.Map.add !forward_map
 				~key:(self#find_exn_inverse ~key:k) ~data:k)
+    method count ~f =
+      Core.Map.count !forward_map ~f
+    method count_inverse ~f =
+      Core.Map.count !reverse_map ~f
+    method counti ~f =
+      Core.Map.counti !forward_map ~f
     method data =
       Core.Map.data !forward_map
     method data_inverse =
@@ -49,7 +55,15 @@ module Bimap = struct
       reverse_map :=
 	(Core.Map.empty ~comparator:(Core.Map.comparator !reverse_map))
 (*    method equal f ~other_bimap =
-      Core.Map.equal f !forward_map !other_fwd_map *)
+      Core.Map.equal f !forward_map !other_fwd_map*)
+    method exists ~f =
+      Core.Map.exists !forward_map ~f
+    method exists_inverse ~f =
+      Core.Map.exists !reverse_map ~f
+    method existsi ~f =
+      Core.Map.existsi !forward_map ~f
+    method existsi_inverse ~f =
+      Core.Map.existsi !reverse_map ~f
     method find ~key =
       Core.Map.find !forward_map key
     method find_inverse ~key =
@@ -76,7 +90,27 @@ module Bimap = struct
     method filteri_inverse ~f =
       let () = reverse_map := (Core.Map.filteri !reverse_map ~f) in
       self#create_forward_map_from_reverse_map
+    method filter_map ~f =
+      let () = forward_map := (Core.Map.filter_map !forward_map ~f) in
+      self#create_inverse_map_from_forward_map
+    method filter_map_inverse ~f =
+      let () = reverse_map := (Core.Map.filter_map !reverse_map ~f) in
+      self#create_forward_map_from_reverse_map
     (*    method forward_map = !forward_map*)
+    method fold ~(init:'a) ~(f:key:'a -> data:'b -> 'a -> 'a) =
+      Core.Map.fold !forward_map ~init ~f
+    method fold_inverse ~(init:'b) ~(f:key:'b -> data:'a -> 'b -> 'b) =
+      Core.Map.fold !reverse_map ~init ~f
+(*    method fold_range_inclusive ~min ~max ~init ~f =
+      Core.Map.fold_range_inclusive !forward_map ~min ~max ~init ~f*)
+    method fold_right ~(init:'a) ~(f:key:'a -> data:'b -> 'a -> 'a) =
+      Core.Map.fold_right !forward_map ~init ~f
+    method fold_right_inverse ~(init:'b) ~(f:key:'b -> data:'a -> 'b -> 'b) =
+      Core.Map.fold_right !reverse_map ~init ~f
+    method for_all ~f =
+      Core.Map.for_all !forward_map ~f
+    method for_all_inverse ~f =
+      Core.Map.for_all !reverse_map ~f
     method is_empty =
       Core.Map.is_empty !forward_map
     method iter_keys ~f =
@@ -137,6 +171,26 @@ module Bimap = struct
       Core.Map.mem !forward_map key
     method mem_inverse key =
       Core.Map.mem !reverse_map key
+    method min_elt =
+      Core.Map.min_elt !forward_map
+    method min_elt_exn =
+      Core.Map.min_elt_exn !forward_map
+    method min_elt_inverse =
+      Core.Map.min_elt !reverse_map
+    method min_elt_exn_inverse =
+      Core.Map.min_elt_exn !reverse_map
+    method max_elt =
+      Core.Map.max_elt !forward_map
+    method max_elt_exn =
+      Core.Map.max_elt_exn !forward_map
+    method max_elt_inverse =
+      Core.Map.max_elt !reverse_map
+    method max_elt_exn_inverse =
+      Core.Map.max_elt_exn !reverse_map
+    method nth int =
+      Core.Map.nth !forward_map int
+    method nth_inverse int =
+      Core.Map.nth !reverse_map int
     method remove ~key =
       let reverse_key = Core.Map.find_exn !forward_map key in 
       let () = forward_map := (Core.Map.remove !forward_map key) in
@@ -152,12 +206,15 @@ module Bimap = struct
       let newvalue = Core.Map.find_exn !forward_map key in 
       let () = reverse_map := (Core.Map.add !reverse_map newvalue key) in
       reverse_map := (Core.Map.remove !reverse_map oldvalue)
+    method to_alist ?key_order () =
+      match Core.Option.is_some key_order with
+      | false -> Core.Map.to_alist !forward_map
+      | true -> Core.Map.to_alist ~key_order:(Core.Option.value_exn key_order) !forward_map
   end
 end 
 
-(*
-    method fold ~init ~f =
-      Core.Map.fold !forward_map ~init ~f
+(*  
+    
     method fold_inverse ~init ~f =
       Core.Map.fold !reverse_map ~init ~f
 
--- src/lib/bimap.mli
+++ src/lib/bimap.mli
@@ -20,9 +20,16 @@ module Bimap :
         method comparator : unit -> ('a, 'c) Core_kernel__.Comparator.t
         method comparator_inverse :
 		 unit -> ('b, 'd) Core_kernel__.Comparator.t*)
+	method count : f:('b -> bool) -> int
+	method count_inverse : f:('a -> bool) -> int
+	method counti : f:(key:'a -> data:'b -> bool) -> int
         method data : 'b list
         method data_inverse : 'a list
 	method empty : unit -> unit
+	method exists : f:('b -> bool) -> bool
+	method exists_inverse : f:('a -> bool) -> bool
+	method existsi : f:(key:'a -> data:'b -> bool) -> bool
+	method existsi_inverse : f:(key:'b -> data:'a -> bool) -> bool
         method find : key:'a -> 'b option
         method find_exn : key:'a -> 'b
         method find_exn_inverse : key:'b -> 'a
@@ -33,8 +40,17 @@ module Bimap :
 	method filteri_inverse : f:(key:'b -> data:'a -> bool) -> unit
 	method filter_keys : f:('a -> bool) -> unit
 	method filter_keys_inverse : f:('b -> bool) -> unit
+	method filter_map : f:('b -> 'b option) -> unit
+	method filter_map_inverse : f:('a -> 'a option) -> unit
 	(*	method forward_map : ('a,'b,_) Core.Map.t*)
-        method is_empty : bool
+	method fold : init:'a -> f:(key:'a -> data:'b -> 'a -> 'a) -> 'a
+	method fold_inverse : init:'b -> f:(key:'b -> data:'a -> 'b -> 'b) -> 'b
+	(*method fold_range_inclusive : min:'a -> max:'a -> init:'b -> f:(key:'a -> data:'b -> 'b) -> 'b*)
+	method fold_right : init:'a -> f:(key:'a -> data:'b -> 'a -> 'a) -> 'a
+	method fold_right_inverse : init:'b -> f:(key:'b -> data:'a -> 'b -> 'b) -> 'b
+	method for_all : f:('b -> bool) -> bool
+	method for_all_inverse : f:('a -> bool) -> bool
+	method is_empty : bool
         method iter :
           f:('b -> unit) -> unit
         method iter_inverse :
@@ -58,9 +74,20 @@ module Bimap :
           f:(key:'b -> data:'a -> 'a) -> unit
         method mem : 'a -> bool
         method mem_inverse : 'b -> bool
+	method min_elt : ('a * 'b) option
+	method min_elt_exn : ('a * 'b)
+	method min_elt_inverse : ('b * 'a) option
+	method min_elt_exn_inverse : ('b * 'a)
+	method max_elt : ('a * 'b) option
+	method max_elt_exn : ('a * 'b)
+	method max_elt_inverse : ('b * 'a) option
+	method max_elt_exn_inverse : ('b * 'a)
+	method nth : int -> ('a * 'b) option
+	method nth_inverse : int -> ('b * 'a) option
         method remove : key:'a -> unit
         method remove_inverse : key:'b -> unit
 	(*	method reverse_map : ('b, 'a, _) Core.Map.t*)
+	method to_alist : ?key_order:[`Increasing | `Decreasing] -> unit -> ('a * 'b) list 
         method update :
           'a -> f:('b option -> 'b) -> unit
       end
