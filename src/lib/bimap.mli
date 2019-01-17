ad0a7f6d67e2c5b83ce91fa8d426b1034ae29227
paul <paul@guerriere>
2019-01-17 18:55:05 -0500

    On the way; drafting a no  core version; both a module (functor) and a class.  Changes to be committed: 	modified:   META 	modified:   Makefile 	modified:   opam 	modified:   src/lib/bimap.ml 	modified:   src/lib/bimap.mli 	copied:     src/lib/bimap.ml -> src/lib/bimap_class.ml

--- src/lib/bimap.mli
+++ src/lib/bimap.mli
@@ -1,96 +1,89 @@
-module Bimap : sig
-  class ['a, 'b] bimap_class :
-	  ('a, 'b, 'c) Core.Map.t ->
-	  ('b, 'a, 'd) Core.Map.t ->
-	  object
-            val mutable forward_map : ('a, 'b, 'c) Core.Map.t ref
-            val mutable reverse_map : ('b, 'a, 'd) Core.Map.t ref
-            method set : key:'a -> data:'b -> unit
-            method set_inverse : key:'b -> data:'a -> unit
-            method change :
-		     key:'a ->
-		     f:('b option -> 'b option) ->
-		     unit
-            method change_inverse :
-		     key:'b ->
-		     f:('a option -> 'a option) ->
-		     unit
-	    (*CANNOT WRITE THESE RIGHT NOW -- 
-        method comparator : unit -> ('a, 'c) Core_kernel__.Comparator.t
-        method comparator_inverse :
-		 unit -> ('b, 'd) Core_kernel__.Comparator.t*)
-	    method count : f:('b -> bool) -> int
-	    method count_inverse : f:('a -> bool) -> int
-	    method counti : f:(key:'a -> data:'b -> bool) -> int
-            method data : 'b list
-            method data_inverse : 'a list
-	    method empty : unit -> unit
-	    method exists : f:('b -> bool) -> bool
-	    method exists_inverse : f:('a -> bool) -> bool
-	    method existsi : f:(key:'a -> data:'b -> bool) -> bool
-	    method existsi_inverse : f:(key:'b -> data:'a -> bool) -> bool
-            method find : key:'a -> 'b option
-            method find_exn : key:'a -> 'b
-            method find_exn_inverse : key:'b -> 'a
-            method find_inverse : key:'b -> 'a option
-	    method filter : f:('b -> bool) -> unit
-	    method filter_inverse : f:('a -> bool) -> unit
-	    method filteri : f:(key:'a -> data:'b -> bool) -> unit
-	    method filteri_inverse : f:(key:'b -> data:'a -> bool) -> unit
-	    method filter_keys : f:('a -> bool) -> unit
-	    method filter_keys_inverse : f:('b -> bool) -> unit
-	    method filter_map : f:('b -> 'b option) -> unit
-	    method filter_map_inverse : f:('a -> 'a option) -> unit
-	    (*	method forward_map : ('a,'b,_) Core.Map.t*)
-	    (*method fold : init:'a -> f:(key:'a -> data:'b -> 'a -> 'a) -> 'a*)
-            method fold : init:'e -> f:(key:'a -> data:'b -> 'e -> 'e) -> 'e
-	    method fold_inverse : init:'e -> f:(key:'b -> data:'a -> 'e -> 'e) -> 'e
-	    (*method fold_range_inclusive : min:'a -> max:'a -> init:'b -> f:(key:'a -> data:'b -> 'b) -> 'b*)
-	    method fold_right : init:'e -> f:(key:'a -> data:'b -> 'e -> 'e) -> 'e
-	    method fold_right_inverse : init:'e -> f:(key:'b -> data:'a -> 'e -> 'e) -> 'e
-	    method for_all : f:('b -> bool) -> bool
-	    method for_all_inverse : f:('a -> bool) -> bool
-	    method is_empty : bool
-            method iter :
-		     f:('b -> unit) -> unit
-            method iter_inverse :
-		     f:('a -> unit) -> unit
-            method iter_keys :
-		     f:('a -> unit) -> unit
-            method iter_keys_inverse :
-		     f:('b -> unit) -> unit
-            method iteri :
-		     f:(key:'a -> data:'b -> unit) -> unit
-            method iteri_inverse :
-		     f:(key:'b -> data:'a -> unit) -> unit
-            method keys : 'a list
-            method keys_inverse : 'b list
-            method length : int
-            method map : f:('b -> 'b) -> unit
-            method map_inverse : f:('a -> 'a) -> unit
-            method mapi :
-		     f:(key:'a -> data:'b -> 'b) -> unit
-            method mapi_inverse :
-		     f:(key:'b -> data:'a -> 'a) -> unit
-            method mem : 'a -> bool
-            method mem_inverse : 'b -> bool
-	    method min_elt : ('a * 'b) option
-	    method min_elt_exn : ('a * 'b)
-	    method min_elt_inverse : ('b * 'a) option
-	    method min_elt_exn_inverse : ('b * 'a)
-	    method max_elt : ('a * 'b) option
-	    method max_elt_exn : ('a * 'b)
-	    method max_elt_inverse : ('b * 'a) option
-	    method max_elt_exn_inverse : ('b * 'a)
-	    method nth : int -> ('a * 'b) option
-	    method nth_inverse : int -> ('b * 'a) option
-            method remove : key:'a -> unit
-            method remove_inverse : key:'b -> unit
-	    (*	method reverse_map : ('b, 'a, _) Core.Map.t*)
-	    method to_alist : ?key_order:[`Increasing | `Decreasing] -> unit -> ('a * 'b) list 
-            method update :
-		     'a -> f:('b option -> 'b) -> unit
-	  end
-(*  type ('a, 'b) t = ('a, 'b) bimap_class
-  val make : ('a, 'b, 'c) Core.Map.t -> ('b, 'a, 'd) Core.Map.t -> ('a, 'b) t*)
-end
+module Bimap :
+  functor (MapModule1 : Map.S) (MapModule2 : Map.S) ->
+    sig
+      val empty : unit -> unit
+      val is_empty : unit -> bool
+      val mem : MapModule1.key -> bool
+      val mem_reverse : MapModule2.key -> bool
+      val add : key:MapModule1.key -> data:MapModule2.key -> unit
+      val add_reverse : key:MapModule2.key -> data:MapModule1.key -> unit
+      val singleton : key:MapModule1.key -> data:MapModule2.key -> unit
+      val singleton_reverse : key:MapModule2.key -> data:MapModule1.key -> unit
+      val remove : key:MapModule1.key -> unit
+      val remove_reverse : key:MapModule2.key -> unit
+      val merge :
+        f:(MapModule1.key ->
+           MapModule2.key option -> 'a option -> MapModule2.key option) ->
+        othermap:'a MapModule1.t -> unit
+      val merge_reverse :
+        f:(MapModule2.key ->
+           MapModule1.key option -> 'a option -> MapModule1.key option) ->
+        othermap:'a MapModule2.t -> unit
+      val union :
+        f:(MapModule1.key ->
+           MapModule2.key -> MapModule2.key -> MapModule2.key option) ->
+        othermap:MapModule2.key MapModule1.t -> unit
+      val union_reverse :
+        f:(MapModule2.key ->
+           MapModule1.key -> MapModule1.key -> MapModule1.key option) ->
+        othermap:MapModule1.key MapModule2.t -> unit
+      val compare :
+        f:(MapModule2.key -> MapModule2.key -> int) ->
+        othermap:MapModule2.key MapModule1.t -> int
+      val compare_reverse :
+        f:(MapModule1.key -> MapModule1.key -> int) ->
+        othermap:MapModule1.key MapModule2.t -> int
+      val equal :
+        f:(MapModule2.key -> MapModule2.key -> bool) ->
+        othermap:MapModule2.key MapModule1.t -> bool
+      val equal_reverse :
+        f:(MapModule1.key -> MapModule1.key -> bool) ->
+        othermap:MapModule1.key MapModule2.t -> bool
+      val filter : f:(MapModule1.key -> MapModule2.key -> bool) -> unit
+      val filter_reverse :
+        f:(MapModule2.key -> MapModule1.key -> bool) -> unit
+      val iter : f:(MapModule1.key -> MapModule2.key -> unit) -> unit
+      val iter_reverse : f:(MapModule2.key -> MapModule1.key -> unit) -> unit
+      val fold : (MapModule1.key -> MapModule2.key -> 'a -> 'a) -> 'a -> 'a
+      val fold_reverse :
+        (MapModule2.key -> MapModule1.key -> 'a -> 'a) -> 'a -> 'a
+      val for_all : (MapModule1.key -> MapModule2.key -> bool) -> bool
+      val for_all_reverse :
+        (MapModule2.key -> MapModule1.key -> bool) -> bool
+      val exists : f:(MapModule1.key -> MapModule2.key -> bool) -> bool
+      val exists_reverse :
+        f:(MapModule2.key -> MapModule1.key -> bool) -> bool
+      val partition :
+        f:(MapModule1.key -> MapModule2.key -> bool) ->
+        MapModule2.key MapModule1.t * MapModule2.key MapModule1.t
+      val partition_reverse :
+        f:(MapModule2.key -> MapModule1.key -> bool) ->
+        MapModule1.key MapModule2.t * MapModule1.key MapModule2.t
+      val cardinal : unit -> int
+      val cardinal_reverse : unit -> int
+      val bindings : unit -> (MapModule1.key * MapModule2.key) list
+      val bindings_reverse : unit -> (MapModule2.key * MapModule1.key) list
+      val min_binding : unit -> MapModule1.key * MapModule2.key
+      val min_binding_reverse : unit -> MapModule2.key * MapModule1.key
+      val max_binding : unit -> MapModule1.key * MapModule2.key
+      val max_binding_reverse : unit -> MapModule2.key * MapModule1.key
+      val choose : unit -> MapModule1.key * MapModule2.key
+      val choose_reverse : unit -> MapModule2.key * MapModule1.key
+      val split :
+        key:MapModule1.key ->
+        MapModule2.key MapModule1.t * MapModule2.key option *
+        MapModule2.key MapModule1.t
+      val split_reverse :
+        key:MapModule2.key ->
+        MapModule1.key MapModule2.t * MapModule1.key option *
+        MapModule1.key MapModule2.t
+      val find : key:MapModule1.key -> MapModule2.key
+      val find_reverse : key:MapModule2.key -> MapModule1.key
+      val map : f:(MapModule2.key -> MapModule2.key) -> unit
+      val map_reverse : f:(MapModule1.key -> MapModule1.key) -> unit
+      val mapi :
+        f:(MapModule1.key -> MapModule2.key -> MapModule2.key) -> unit
+      val mapi_reverse :
+        f:(MapModule2.key -> MapModule1.key -> MapModule1.key) -> unit
+      val set_forward_map : map:(MapModule2.key MapModule1.t) -> unit
+    end
