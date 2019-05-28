c4c3c11be7803cc046d825d6c8a93d72395495f5
paul <paul@guerriere>
2019-05-28 18:19:11 -0400

    Changes to be committed: 	modified:   src/lib/bimap_single_module.ml 	modified:   src/lib/bimap_single_module.mli

--- src/lib/bimap_single_module.ml
+++ src/lib/bimap_single_module.ml
@@ -39,7 +39,7 @@ module Bimap_single_module(MapModule1 : Map.S)(MapModule2 : Map.S) = struct
     else if MapModule1.mem data t.fwdmap then
       let new_forward_map = MapModule1.remove data t.fwdmap in
       let new_reverse_map = MapModule2.add key data t.revmap in 
-      let new_forward_map = MapModule1.add data key t.fwdmap in
+      let new_forward_map = MapModule1.add data key new_forward_map in
       { fwdmap=new_forward_map; revmap=new_reverse_map }
     else
       let new_reverse_map = MapModule2.add key data t.revmap in
@@ -51,74 +51,76 @@ module Bimap_single_module(MapModule1 : Map.S)(MapModule2 : Map.S) = struct
   let singleton_reverse t ~key ~data =
     let empty_t = empty () in
     add_reverse empty_t ~key ~data
-  let create_reverse_map_from_forward_map t =
-    let new_reverse_map = MapModule2.empty in
-    let new_t = { fwdmap=t.fwdmap; revmap=new_reverse_map } in
-    let bindingslist = MapModule1.bindings in
-    let rec helper bindings typet =
-      match bindings with
-      | [] -> t
-      | h :: t ->
-         let newt = add_reverse typet ~key:(snd h) ~data:(fst h) in
-         helper t newt in
-    helper bindingslist new_t
-  let create_forward_map_from_reverse_map t =
-    let new_forward_map = MapModule1.empty in
-    let new_t = {fwdmap=new_forward_map; revmap=t.revmap} in 
-    MapModule2.iter
-      (fun k v ->
-	add new_t ~key:v ~data:k) t.revmap
-(*  let remove ~key =
-    let value = MapModule1.find key !forward_map in 
-    let () = forward_map := MapModule1.remove key !forward_map in
-    reverse_map := MapModule2.remove value !reverse_map
-  let remove_reverse ~key =
-    let fwd_value = MapModule2.find key !reverse_map in 
-    let () = reverse_map := MapModule2.remove key !reverse_map in
-    forward_map := MapModule1.remove fwd_value !forward_map
-  let get_forward_map () =
-    let newmap = ref MapModule1.empty in 
-    let () = MapModule1.iter
-               (fun key value -> newmap := MapModule1.add key value !newmap) !forward_map in
-    !newmap;;
-  let get_reverse_map () =
-    let newmap = ref MapModule2.empty in 
-    let () = MapModule2.iter
-               (fun key value -> newmap := MapModule2.add key value !newmap) !reverse_map in
-    !newmap;;
-  let merge ~f ~othermap =
-    let () = forward_map := MapModule1.merge f !forward_map othermap in
-    create_reverse_map_from_forward_map ()
-  let merge_reverse ~f ~othermap =
-    let () = reverse_map := MapModule2.merge f !reverse_map othermap in
-    create_forward_map_from_reverse_map ()
+  let create_reverse_map_from_forward_map ~forward_map =
+    let newrmap = MapModule2.empty in
+    let newrmapref = ref newrmap in 
+    let () = MapModule1.iter 
+	       (fun key data ->
+	         newrmapref :=
+                   (MapModule2.add data key !newrmapref))
+               forward_map in
+    !newrmapref
+  let create_forward_map_from_reverse_map ~reverse_map =
+    let newfmap = MapModule1.empty in
+    let newfmapref = ref newfmap in 
+    let () = MapModule2.iter 
+	       (fun key data ->
+		 newfmapref :=
+                   MapModule1.add data key !newfmapref) reverse_map in
+    !newfmapref
+
+  let remove t ~key =
+    let value = MapModule1.find key t.fwdmap in 
+    let new_forward_map = MapModule1.remove key t.fwdmap in
+    let new_reverse_map = MapModule2.remove value t.revmap in 
+    { fwdmap=new_forward_map ; revmap=new_reverse_map}
+  let remove_reverse t ~key =
+    let fwd_value = MapModule2.find key t.revmap in 
+    let new_reverse_map = MapModule2.remove key t.revmap in
+    let new_forward_map = MapModule1.remove fwd_value t.fwdmap in
+    { fwdmap=new_forward_map ; revmap=new_reverse_map }
+  let get_forward_map t = t.fwdmap
+  let get_reverse_map t = t.revmap
+  let merge t ~f ~othermap =
+    let new_forward_map = MapModule1.merge f t.fwdmap othermap in
+    let new_rev_map = create_reverse_map_from_forward_map new_forward_map in
+    { fwdmap=new_forward_map; revmap=new_rev_map }
+  let merge_reverse t ~f ~othermap =
+    let new_reverse_map = MapModule2.merge f t.revmap othermap in
+    let new_fwd_map = create_forward_map_from_reverse_map ~reverse_map:new_reverse_map in
+    { fwdmap=new_fwd_map; revmap=new_reverse_map }
 (*  let union ~f ~othermap =
     let () = forward_map := MapModule1.union f !forward_map othermap in
     create_reverse_map_from_forward_map ()
   let union_reverse ~f ~othermap =
     let () = reverse_map := MapModule2.union f !reverse_map othermap in
     create_forward_map_from_reverse_map ()*)
-  let compare ~f ~othermap =
-    MapModule1.compare f !forward_map othermap
-  let compare_reverse ~f ~othermap =
-    MapModule2.compare f !reverse_map othermap
-  let equal ~f ~othermap =
-    MapModule1.equal f !forward_map othermap
-  let equal_reverse ~f ~othermap =
-    MapModule2.equal f !reverse_map othermap
-  let filter ~f =
-    let () = forward_map := (MapModule1.filter f !forward_map) in
-    create_reverse_map_from_forward_map ()
-  let filter_reverse ~f =
-    let () = reverse_map := (MapModule2.filter f !reverse_map) in
-    create_forward_map_from_reverse_map ()
-  let iter ~f =
-    let () = MapModule1.iter f !forward_map in
-    create_reverse_map_from_forward_map ()
-  let iter_reverse ~f =
-    let () = MapModule2.iter f !reverse_map in
-    create_forward_map_from_reverse_map ()    
-  let fold ~f = 
+  let compare t ~f ~othermap =
+    MapModule1.compare f t.fwdmap othermap
+  let compare_reverse t ~f ~othermap =
+    MapModule2.compare f t.revmap othermap
+  let equal t ~f ~othermap =
+    MapModule1.equal f t.fwdmap othermap
+  let equal_reverse t ~f ~othermap =
+    MapModule2.equal f t.revmap othermap
+  let filter t ~f =
+    let new_forward_map = (MapModule1.filter f t.fwdmap) in
+    let new_rev_map = create_reverse_map_from_forward_map new_forward_map in
+    { fwdmap=new_forward_map; revmap=new_rev_map }
+  let filter_reverse t ~f =
+    let new_reverse_map = (MapModule2.filter f t.revmap) in
+    let new_fwd_map = create_forward_map_from_reverse_map new_reverse_map in
+    { fwdmap=new_fwd_map; revmap=new_reverse_map }
+  (*FIX THIS -- iter mutates the map...function should return unit...modify the revmap*)
+  let iter t ~f =
+    let () = MapModule1.iter f t.fwdmap in
+    let new_rev_map = create_reverse_map_from_forward_map t.fwdmap in
+    { fwdmap=t.fwdmap; revmap=new_rev_map }
+  let iter_reverse t ~f =
+    let () = MapModule2.iter f t.revmap in
+    let new_fwd_map = create_forward_map_from_reverse_map t.revmap in
+    { fwdmap=new_fwd_map; revmap=t.revmap }
+(*  let fold ~f = 
     MapModule1.fold f !forward_map
   let fold_reverse ~f =
     MapModule2.fold f !reverse_map
--- src/lib/bimap_single_module.mli
+++ src/lib/bimap_single_module.mli
@@ -11,42 +11,42 @@ sig
   val add_reverse : t -> key:MapModule2.key -> data:MapModule1.key -> t
   val singleton : t -> key:MapModule1.key -> data:MapModule2.key -> t
   val singleton_reverse : t -> key:MapModule2.key -> data:MapModule1.key -> t
-(*  val remove : key:MapModule1.key -> unit
-  val remove_reverse : key:MapModule2.key -> unit
+  val remove : t -> key:MapModule1.key -> t
+  val remove_reverse : t-> key:MapModule2.key -> t
   (*Needed to get access to the underlying map for use by 
         merge function. Better way to do this?*)
-  val get_forward_map : unit -> MapModule2.key MapModule1.t
-  val get_reverse_map : unit -> MapModule1.key MapModule2.t
-  val merge :
-    f:(MapModule1.key ->
-       MapModule2.key option -> 'a option -> MapModule2.key option) ->
-    othermap:'a MapModule1.t -> unit
-  val merge_reverse :
-    f:(MapModule2.key ->
-       MapModule1.key option -> 'a option -> MapModule1.key option) ->
-    othermap:'a MapModule2.t -> unit
+  val get_forward_map : t -> MapModule2.key MapModule1.t
+  val get_reverse_map : t -> MapModule1.key MapModule2.t
+  val merge : t ->
+              f:(MapModule1.key ->
+                 MapModule2.key option -> 'a option -> MapModule2.key option) ->
+              othermap:'a MapModule1.t -> t
+  val merge_reverse : t ->
+                      f:(MapModule2.key ->
+                         MapModule1.key option -> 'a option -> MapModule1.key option) ->
+                      othermap:'a MapModule2.t -> t
   (*val union : (ModuleA.key -> ModuleB.key -> ModuleB.key -> ModuleB.key option) ->
                   MapModule2.key MapModule1.t -> MapModule2.key MapModule1.t -> unit*)
   val compare :
-    f:(MapModule2.key -> MapModule2.key -> int) ->
+    t -> f:(MapModule2.key -> MapModule2.key -> int) ->
     othermap:MapModule2.key MapModule1.t -> int
   val compare_reverse :
-    f:(MapModule1.key -> MapModule1.key -> int) ->
+    t -> f:(MapModule1.key -> MapModule1.key -> int) ->
     othermap:MapModule1.key MapModule2.t -> int
   val equal :
-    f:(MapModule2.key -> MapModule2.key -> bool) ->
+    t -> f:(MapModule2.key -> MapModule2.key -> bool) ->
     othermap:MapModule2.key MapModule1.t -> bool
   val equal_reverse :
-    f:(MapModule1.key -> MapModule1.key -> bool) ->
+    t -> f:(MapModule1.key -> MapModule1.key -> bool) ->
     othermap:MapModule1.key MapModule2.t -> bool
-  val filter : f:(MapModule1.key -> MapModule2.key -> bool) -> unit
+  val filter : t -> f:(MapModule1.key -> MapModule2.key -> bool) -> t
   val filter_reverse :
-    f:(MapModule2.key -> MapModule1.key -> bool) -> unit
+    t -> f:(MapModule2.key -> MapModule1.key -> bool) -> t
   (*---------------Not testing these 2 right now---------------*)
-  val iter : f:(MapModule1.key -> MapModule2.key -> unit) -> unit
-  val iter_reverse : f:(MapModule2.key -> MapModule1.key -> unit) -> unit
+  val iter : t -> f:(MapModule1.key -> MapModule2.key -> unit) -> unit
+  val iter_reverse : t -> f:(MapModule2.key -> MapModule1.key -> unit) -> unit
   (*-----------------------------------------------------------*)
-  val fold : f:(MapModule1.key -> MapModule2.key -> 'a -> 'a) -> 'a -> 'a
+(*  val fold : f:(MapModule1.key -> MapModule2.key -> 'a -> 'a) -> 'a -> 'a
   val fold_reverse :
     f:(MapModule2.key -> MapModule1.key -> 'a -> 'a) -> 'a -> 'a
   val for_all : f:(MapModule1.key -> MapModule2.key -> bool) -> bool
